057985f9c885d70f4707853ce029e3363d0ede5c
Andrew Khan <andre-khan@yandex.ru>
2023-03-05 21:56:41 +0300

    дз6

---
+++ homework/HW6/Program.cs
@@ -0,0 +1,70 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0;i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+
+// void ShowArray (int[] array)
+// {
+//     for(int i = 0;i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void PositiveNumber (int [] array)
+// {
+//     int number = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//       if (array[i] >= 0) number ++;
+//     }
+//   Console.WriteLine($"количество чисел больше нуля = {number}");  
+// }
+// Console.WriteLine("Input array size");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input minimal value of array element");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input max value of array element");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+
+
+// int[] myArray = CreateRandomArray(size, minValue, maxValue);
+// ShowArray(myArray);
+// PositiveNumber(myArray);
+
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения
+// двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void Intersection(double b1, double k1, double b2, double k2)
+{
+    double x = Math.Round((b2-b1)/(k1-k2),2);
+    double y = Math.Round(k1 * x + b1, 2);
+    if (k1 == k2) Console.WriteLine("Линии параллельны");
+    else Console.WriteLine($"Линии пересекаются в точках x={x} y={y}");
+}
+Console.WriteLine("Введите значение b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Intersection(b1,k1,b2,k2);
